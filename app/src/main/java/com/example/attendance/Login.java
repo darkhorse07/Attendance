13cb8a7fb4806ffdf5d95fa0b3ff7e9d6a8ca6af
darkhorse07 <bcs_201846@iiitm.ac.in>
2020-07-20 21:42:14 +0530

    Login UI

---
+++ app/src/main/java/com/example/attendance/Login.java
@@ -0,0 +1,40 @@
+package com.example.attendance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Login extends AppCompatActivity {
+
+    int code;
+
+    EditText emailTextView;
+    EditText passwordTextView;
+
+    public void login(View view) {
+
+        /**/
+    }
+
+    public void signup(View view) {
+
+        /**/
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        emailTextView = (EditText) findViewById(R.id.emailTextView);
+        passwordTextView = (EditText) findViewById(R.id.passwordTextView);
+
+        Intent intent = getIntent();
+        code = intent.getIntExtra("code", 0);
+    }
+}
--- app/src/main/java/com/example/attendance/MainActivity.java
+++ app/src/main/java/com/example/attendance/MainActivity.java
@@ -1,10 +1,26 @@
 package com.example.attendance;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    public void studentLogin(View view) {
+
+        Intent intent = new Intent(getApplicationContext(), Login.class);
+        intent.putExtra("code", 1);
+        startActivity(intent);
+    }
+
+    public void facultyLogin(View view) {
+
+        Intent intent = new Intent(getApplicationContext(), Login.class);
+        intent.putExtra("code", 2);
+        startActivity(intent);
+    }
 
 
     @Override
