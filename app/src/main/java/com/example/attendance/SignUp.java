81496cbd966fa635692a71f0c7f1add1693beddf
darkhorse07 <bcs_201846@iiitm.ac.in>
2020-07-22 20:58:00 +0530

    Register UI

--- app/src/main/java/com/example/attendance/SignUp.java
+++ app/src/main/java/com/example/attendance/SignUp.java
@@ -6,37 +6,34 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 
-public class Login extends AppCompatActivity {
+public class SignUp extends AppCompatActivity {
 
     int code;
 
     EditText emailTextView;
     EditText passwordTextView;
+    EditText firstNameTextView;
+    EditText secondNameTextView;
 
-    public void login(View view) {
+    public void register(View view) {
 
         /**/
-    }
-
-    public void signup(View view) {
-
-        Intent intent = new Intent(getApplicationContext(), SignUp.class);
-        intent.putExtra("code", code);
-        startActivity(intent);
-
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_sign_up);
 
         emailTextView = (EditText) findViewById(R.id.emailTextView);
         passwordTextView = (EditText) findViewById(R.id.passwordTextView);
+        firstNameTextView = (EditText) findViewById(R.id.firstNameTextView);
+        secondNameTextView = (EditText) findViewById(R.id.secondNameTextView);
 
         Intent intent = getIntent();
         code = intent.getIntExtra("code", 0);
+
+
     }
 }
