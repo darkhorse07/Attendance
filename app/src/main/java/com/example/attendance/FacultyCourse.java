dc8fb78a8562447a2213b2f40b821b9bf7e3946c
darkhorse07 <bcs_201846@iiitm.ac.in>
2020-07-23 20:42:13 +0530

    Partial FacultyHome, FacultyCourse

---
+++ app/src/main/java/com/example/attendance/FacultyCourse.java
@@ -0,0 +1,33 @@
+package com.example.attendance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+
+public class FacultyCourse extends AppCompatActivity {
+
+
+    public void attendance(View view) {
+
+        /**/
+    }
+
+    public void checkRecord(View view) {
+
+        /**/
+    }
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faculty_course);
+
+        Intent intent = getIntent();
+    }
+}
--- app/src/main/java/com/example/attendance/FacultyHome.java
+++ app/src/main/java/com/example/attendance/FacultyHome.java
@@ -1,8 +1,13 @@
 package com.example.attendance;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -15,6 +20,37 @@ public class FacultyHome extends AppCompatActivity {
 
     TextView welcomeTextView;
     ListView courseListView;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.faculty_home_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        switch (item.getItemId()) {
+
+            case R.id.addNewCourse : /**/ break;
+
+            case R.id.help: /**/ break;
+
+            case R.id.logout: {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                break;
+            }
+
+            default: return false;
+        }
+
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +73,9 @@ public class FacultyHome extends AppCompatActivity {
             @Override
 
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                /**/
+
+                Intent intent = new Intent(getApplicationContext(), FacultyCourse.class);
+                startActivity(intent);
 
             }
         });
