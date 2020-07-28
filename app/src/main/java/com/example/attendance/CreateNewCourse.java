65d9524d0d9e53168192ab1be318757e2ed0b7b4
darkhorse07 <bcs_201846@iiitm.ac.in>
2020-07-28 11:18:13 +0530

    CreateNewCourse (Faculty) added

---
+++ app/src/main/java/com/example/attendance/CreateNewCourse.java
@@ -0,0 +1,13 @@
+package com.example.attendance;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateNewCourse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_new_course);
+    }
+}
