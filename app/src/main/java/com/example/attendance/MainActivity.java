14410adb15dac46d4af0e57dd8db343866a884ad
darkhorse07 <bcs_201846@iiitm.ac.in>
2020-08-02 13:24:58 +0530

    Main Activity UI complete

--- app/src/main/java/com/example/attendance/MainActivity.java
+++ app/src/main/java/com/example/attendance/MainActivity.java
@@ -4,9 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.Spanned;
 import android.view.View;
+import android.widget.TextView;
+
+import static android.text.Html.FROM_HTML_MODE_LEGACY;
+import static android.text.Layout.JUSTIFICATION_MODE_INTER_WORD;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView aboutTextView;
 
     public void studentLogin(View view) {
 
@@ -27,5 +35,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        aboutTextView = (TextView) findViewById(R.id.aboutTextView);
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+
+            String text = getString(R.string.about_MarkME);
+            Spanned styledText = Html.fromHtml(text, FROM_HTML_MODE_LEGACY);
+            aboutTextView.setText(styledText);
+            aboutTextView.setJustificationMode(JUSTIFICATION_MODE_INTER_WORD);
+
+        }
     }
 }
